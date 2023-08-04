5cf14f1550bbed629cbc82d4522c3443d6839690
busani9 <31929250+busani9@users.noreply.github.com>
2023-08-04 15:13:01 +0100

    Update variables.tf

--- terr_vpc_variables-20230424T020931Z-001/terr_vpc_variables/variables.tf
+++ terr_vpc_variables-20230424T020931Z-001/terr_vpc_variables/variables.tf
@@ -13,3 +13,17 @@ default = "ap-south-1b"
 variable subnet2_cidr_block {
 default = "192.168.2.0/24"
 }
+
+variable terraform-key-pair {
+default = "amarsampleaug0423"
+}
+
+variable ami {
+default = "ami-021f7978361c18b01"
+}
+
+variable instance_type {
+default = "t2.micro"
+}
+
+
